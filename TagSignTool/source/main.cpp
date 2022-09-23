a3aa33c485c2fe301f554ccdd14af2a16e49abd0
LeeFoulger <theTwist84@users.noreply.github.com>
2022-09-23 22:14:45 +0100

    Update main.cpp

--- TagSignTool/source/main.cpp
+++ TagSignTool/source/main.cpp
@@ -1,13 +1,10 @@
 #include "cache/cache_files.hpp"
+#include "cache/physical_memory_map.hpp"
 #include "cseries/cseries_strings.hpp"
 #include "tag_files/files.hpp"
 #include "tag_files/files_windows.hpp"
 
 #include <stdio.h>
-
-// 256 MB max tags file size
-const unsigned long tags_file_buffer_size = 1 << 28;
-unsigned char tags_file_buffer[tags_file_buffer_size]{};
 
 char usage[] = R"(
 Usage:
@@ -16,6 +13,8 @@ Usage:
 Example:
 	TagSignTool.exe "C:\Games\Halo Online (1.106708 2015-03-20)\maps\tags.dat"
 )";
+
+int main_body(char const* tags_path);
 
 int main(int argc, char* argv[])
 {
@@ -26,10 +25,22 @@ int main(int argc, char* argv[])
 		return 1;
 	}
 
-	char const* tags_path = argv[1];
+	physical_memory_initialize();
+
+	int result = main_body(argv[1]);
+
+	physical_memory_free();
+
+	return result;
+}
+
+int main_body(char const* tags_path)
+{
 	s_file_reference tags_file{};
 	unsigned long tags_file_size = 0;
+	void* tags_file_buffer = nullptr;
 	cache_file_tags_header* tags_header = nullptr;
+	double elapsed_time = 0.0;
 
 	if (!string_ends_with(tags_path, "tags.dat"))
 		return 2;
@@ -39,12 +50,11 @@ int main(int argc, char* argv[])
 		return 3;
 
 	file_get_size(&tags_file, &tags_file_size);
-	if (!file_read_into_buffer(&tags_file, (void**)&tags_file_buffer, tags_file_buffer_size))
+	tags_file_buffer = physical_memory_malloc(tags_file_size);
+	if (!file_read_into_buffer(&tags_file, &tags_file_buffer, tags_file_size))
 		return 4;
 
-	tags_header = reinterpret_cast<cache_file_tags_header*>(tags_file_buffer);
-
-	double elapsed_time = 0.0;
+	tags_header = static_cast<cache_file_tags_header*>(tags_file_buffer);
 	tags_header->sign_instances(&elapsed_time);
 
 	printf("Signing all instances took %.2lf seconds.", elapsed_time);
@@ -52,7 +62,7 @@ int main(int argc, char* argv[])
 	file_reference_set_name(&tags_file, "tags_signed.dat");
 	file_create(&tags_file);
 
-	if (!file_write_from_buffer(&tags_file, tags_file_size, tags_file_buffer))
+	if (!file_write_from_buffer(&tags_file, tags_file_size, tags_header))
 		return 5;
 
 	return 0;
